5cf936a91c83e5f7ebf4367ff23e32b8f45c95bb
matanel-mor <matanelmm1997@gmail.com>
2025-02-03 14:55:58 +0200

    add retry + improve logs

--- src/tests/example/login.spec.ts
+++ src/tests/example/login.spec.ts
@@ -1,7 +1,9 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 import LoginPage from '../../pages/example/loginPage';
+import logger from '../../utils/Logger';
 
 test('login', async ({ page }) => {
+      logger.info("----------- Starting login test -----------");
       const loginPage = new LoginPage(page);
       const homePage = await loginPage.login();
       await homePage.assertNavigationToHomePage();
